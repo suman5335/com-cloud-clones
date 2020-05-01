15f8eb0cbbbeb5916e858325549995d6b8eea29d
Michael Dudley <sound.plus.fury@gmail.com>
2020-05-01 09:51:28 -0700

    built out more test components

---
+++ force-app/main/default/lwc/testCard/testCard.js
@@ -0,0 +1,6 @@
+import { LightningElement, track, api } from 'lwc';
+
+export default class TestCard extends LightningElement {
+
+
+}
--- force-app/main/default/lwc/testComponent1/testComponent1.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class TestComponent1 extends LightningElement {}
