c764fbf69104e3a5ec7cae6d574bff7fb901438b
Michael Dudley <sound.plus.fury@gmail.com>
2020-05-11 14:20:50 -0700

    styled and worked with function

--- force-app/main/default/lwc/configCard3slds/configCard3slds.js
+++ force-app/main/default/lwc/configCard3slds/configCard3slds.js
@@ -1,12 +1,11 @@
 import { LightningElement, api, track} from 'lwc';
 
-export default class DesignComponentLWCDemo extends LightningElement {
+export default class configCard3slds extends LightningElement {
     // tracking attributes values
     @track flag = false;
     @api picFlip = false;
     @api cardHeadline;
     @api cardText;
-    @api backgroundColor;
     @api imageURL;
     @api webURL;
     @api descriptionURL;
